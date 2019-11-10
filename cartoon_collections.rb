925f845b024f2e82855a51c886171e8278d0d3f8
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-10 06:23:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ end
 def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-    ingredients.include?(cheese_types)
+  ingredients.find do |cheese| 
+    cheese.include?(cheese_types)
   end
 end
 
